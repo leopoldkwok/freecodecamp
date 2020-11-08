406ef1bd4d08c0a9f6278bdf198aebf3f45b6659
leopoldkwok <leopoldkwok@gmail.com>
2020-11-08 10:50:03 +0000

    Word Blanks

---
+++ 02-JavaScript-Algorithms-and-Data-Structures-Certification/01-Basic-JavaScript/36-Word-Blanks.js
@@ -0,0 +1,8 @@
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+
+// Only change code below this line
+var wordBlanks = "This " + myNoun + " is " + myAdjective + ", and we " + myVerb + " away " +  myAdverb + "."; // Change this line
+// Only change code above this line
