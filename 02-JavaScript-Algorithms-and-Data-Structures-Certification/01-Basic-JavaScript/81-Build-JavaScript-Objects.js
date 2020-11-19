282c8046e27428f5ba74c423f99c9cb731705ad1
leopoldkwok <leopoldkwok@gmail.com>
2020-11-19 22:17:16 +0000

    Build JavaScript Objects

---
+++ 02-JavaScript-Algorithms-and-Data-Structures-Certification/01-Basic-JavaScript/81-Build-JavaScript-Objects.js
@@ -0,0 +1,10 @@
+var myDog = {
+    // Only change code below this line
+        "name": "Lassie",
+        "legs": 4,
+        "tails": 1,
+        "friends": ["Lion", "Kev"]
+    
+    // Only change code above this line
+    };
+    
