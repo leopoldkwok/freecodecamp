19678a9a3dd473e27da3e986a4213745879f41f3
leopoldkwok <leopoldkwok@gmail.com>
2020-11-25 09:15:02 +0000

    Manipulating Complex Objects

---
+++ 02-JavaScript-Algorithms-and-Data-Structures-Certification/01-Basic-JavaScript/90-Manipulating-Complex-Objects.js
@@ -0,0 +1,26 @@
+var myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "ColdPlay",
+      "title": "Yellow",
+      "release_year": 2000,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    }
+    // Add a record here
+  ];
+  
