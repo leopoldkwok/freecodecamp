8a734ddfad942d41c151739007ce7811aa3e98d2
leopoldkwok <leopoldkwok@gmail.com>
2020-12-13 10:19:28 +0000

    Copy Array Items Using slice()

---
+++ 02-JavaScript-Algorithms-and-Data-Structures-Certification/05-Basic-Data-Structures/07-Copy-Array-Items-Using-slice().js
@@ -0,0 +1,9 @@
+function forecast(arr) {
+    // Only change code below this line
+    let todaysWeather = arr.slice(2, 4);
+    return todaysWeather;
+  }
+  
+  // Only change code above this line
+  console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+  
