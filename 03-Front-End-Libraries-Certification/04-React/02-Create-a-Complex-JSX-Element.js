e7385a5332bcb04263cdc2db3798bee54f27c177
leopoldkwok <leopoldkwok@gmail.com>
2020-12-20 20:46:20 +0000

    Create a Complex JSX Element

---
+++ 03-Front-End-Libraries-Certification/04-React/02-Create-a-Complex-JSX-Element.js
@@ -0,0 +1,11 @@
+const JSX = (
+    <div>
+        <h1>H1</h1>
+        <p>Paragraph One</p>
+        <ul>
+            <li>LI</li>
+            <li>LI</li>
+            <li>LI</li>
+        </ul>
+    </div>
+)
