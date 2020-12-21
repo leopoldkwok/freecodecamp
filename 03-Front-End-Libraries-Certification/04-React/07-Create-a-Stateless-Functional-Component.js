01aee44cfb78e9415083ac76a775fac72f1474c2
leopoldkwok <leopoldkwok@gmail.com>
2020-12-21 21:16:40 +0000

    Create a Stateless Functional Component

---
+++ 03-Front-End-Libraries-Certification/04-React/07-Create-a-Stateless-Functional-Component.js
@@ -0,0 +1,11 @@
+const MyComponent = function() {
+    // Change code below this line
+    return (
+      <div>
+        Hello World
+      </div>
+    )
+  
+  
+    // Change code above this line
+  }
