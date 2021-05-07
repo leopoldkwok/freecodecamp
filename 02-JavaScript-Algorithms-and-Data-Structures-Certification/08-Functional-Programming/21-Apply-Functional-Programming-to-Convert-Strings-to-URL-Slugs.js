b1235bf5ff4490695c7c82f1974f3bd53637bba9
leopoldkwok <leopoldkwok@gmail.com>
2021-05-07 23:49:00 +0100

    Apply Functional Programming to Convert Strings t

---
+++ 02-JavaScript-Algorithms-and-Data-Structures-Certification/08-Functional-Programming/21-Apply-Functional-Programming-to-Convert-Strings-to-URL-Slugs.js
@@ -0,0 +1,11 @@
+// Only change code below this line
+function urlSlug(title) {
+  return title
+    .split(' ')
+    .filter((substr) => substr !== '')
+    .join('-')
+    .toLowerCase();
+}
+// Only change code above this line
+
+urlSlug(' Winter Is  Coming');
